8a026b7a6b6de8225c7d704925dda6585a392ebb
ukaracivio <umit.karacivi@outlook.com>
2023-12-18 14:00:02 +0300

    Some changes

--- P01_CF/Controllers/HomeController.cs
+++ P01_CF/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using P01_CF.Models;
 using P01_CF.Entities;
 using System.Diagnostics;
 using Person = P01_CF.Entities.Person;
+using P01_CF.Data;
 
 namespace P01_CF.Controllers
 {
@@ -18,16 +19,21 @@ namespace P01_CF.Controllers
 
         public IActionResult Index()
         {
+            // Sayfaya ilk gelecek kısımda.. Index sayfası gelmeden önce örnek veriyi burada yaratacak.Yarattıktan sonra da görmek için İndex View2a gidecek.
+            StudentDbContext dbContext=new StudentDbContext();
+
             // Sample Data ??????
             Faker<Person> fakerPerson; // Bogus'un Faker isimli classı
 
             var personList = new List<Person>();
+            // ?? DB tarafına gönderilme...
 
+            // Deneme olması açısından 10 tane dummy data ..for çevrimiyle ve bunu bir listeye yerleştirerek.
 
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 20; i++)
             {
-                fakerPerson = new Faker<Person>()
-                    .RuleFor(p => p.PersonId, fd => fd.Random.Int(1, 200))
+                fakerPerson = new Faker<Person>("tr") // Regional tr Türkiye
+                    //.RuleFor(p => p.PersonId, fd => fd.Random.Int(1, 200))
                     .RuleFor(p => p.PFName, fd => fd.Name.FirstName())
                     .RuleFor(p => p.PLName, fd => fd.Name.LastName())
                     .RuleFor(p => p.PEMail, fd => fd.Internet.Email())
@@ -37,14 +43,29 @@ namespace P01_CF.Controllers
                     .RuleFor(p => p.PCity, fd => fd.Address.City())
                     .RuleFor(p => p.PGender, fd => fd.PickRandom<GenderEnum>());
 
-                var person = fakerPerson.Generate();
+                var person = fakerPerson.Generate(); // fake data yaratılıyor.
 
-                personList.Add(person);
+                personList.Add(person); // yaratılan data bir listeye ekleniyor
 
+                dbContext.Add<Person>(person);
+
+                dbContext.SaveChanges();
+
+                
             }
 
 
-            return View(personList);
+            //var std = new Student()
+            //{
+            //    FirstName = "Bill",
+            //    LastName = "Gates"
+            //};
+            //context.Students.Add(std);
+
+            // or
+            // context.Add<Student>(std);
+
+            return View(personList); // Home/Index sayfasına gönderiliyor.
         }
 
         public IActionResult Privacy()
