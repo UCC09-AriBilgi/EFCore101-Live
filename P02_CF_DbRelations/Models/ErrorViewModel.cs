2fe51c70f01b4e42f54c58b9b94967e08b079688
ukaracivio <umit.karacivi@outlook.com>
2023-12-19 10:24:28 +0300

    Project creation

---
+++ P02_CF_DbRelations/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P02_CF_DbRelations.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
